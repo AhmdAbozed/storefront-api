518578747117f6494bc49c43a4fba24bbcd89e57
ahmd hassan abozed <ahmdabozed88@gmail.com>
2022-07-31 01:32:13 +0200

    added scripts for creation of databases and migrations

--- src/database.ts
+++ src/database.ts
@@ -4,20 +4,35 @@ import pg from "pg";
 
 const { Pool } = pg;
 
+let client= new Pool();
+
 dotenv.config()
 
 const {
     PG_HOST,
     PG_DB,
+    PG_TEST_DB,
     PG_USER,
-    PG_PASS
+    PG_PASS,
+    ENV
 } = process.env
-
-const client = new Pool({
+if (ENV == 'dev'){
+    console.log("hailing from dev database.ts")
+ client = new Pool({
     host: PG_HOST,
     database: PG_DB,
     user: PG_USER,
     password: PG_PASS
 })
+}
+
+if (ENV == 'test'){
+     client = new Pool({
+        host: PG_HOST,
+        database: PG_TEST_DB,
+        user: PG_USER,
+        password: PG_PASS
+    })
+}
 
 export default client
